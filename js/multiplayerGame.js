5a02674184bf5c81afd9e475595ad813d66938ff
luka-pro <149896475+Luka-hash-go@users.noreply.github.com>
2024-12-26 18:07:55 +0100

    initial comit

---
+++ js/multiplayerGame.js
@@ -0,0 +1,20 @@
+   // Single Player
+  function startMultiPlayer(userSquares) {
+      this.userSquares = userSquares
+/*
+      startButton.addEventListener('click', () => {
+      setupButtons.style.display = 'none'*/
+      playGameMulti()
+    //})
+  }
+
+  
+
+
+
+ 
+
+
+
+
+
